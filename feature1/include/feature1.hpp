d10b805e7845ec1091bdc772d61ee7e726122ea8
ladeveze <ladeveze@pc-vincent>
2021-08-31 18:49:45 +0200

    Directory tree

---
+++ feature1/include/feature1.hpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021, Detection Technology Plc.
+ *
+ * feature1.hpp - <4 words of definition for the feature. Ex: Camera object interface>
+ *
+ * @author:
+ * @date:
+ *
+ */
+#ifndef __FEATURE1_HPP__
+#define __FEATURE1_HPP__
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+namespace feature1 {
+
+
+/*
+ *  E N U M S
+ */
+enum feat1 {
+    foo         = 1,
+    bar         = 2,
+    foobar      = 3,
+};
+
+
+/*
+ *  feature1a class definition
+ */
+class feature1a {
+
+public:
+    feature1a();
+    ~feature1a();
+
+}
+
+
+} /* namespace feature1 */
+
+#endif /* __FEATURE1_HPP__  */
