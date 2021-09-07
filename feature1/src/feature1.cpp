216cefeb4497a04d150a616153910738c69f0e37
Vincent Ladeveze <vladeveze@gmail.com>
2021-09-07 11:11:30 +0200

    Introduction of VS Code, remaining bugs in C++

--- feature1/src/feature1.cpp
+++ feature1/src/feature1.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "feature1.hpp"
 
+// using namespace feature1;
 
-
-int feature1a::displayHelloDetector() {
+// int feature1a::displayHelloDetector() {
+int displayHelloDetector() {
     std::cout << "Hello ME detector" << std::endl;
 
-    std::cout << "if you are Guilaume Gey:  encule Thérèse !!!" << std::endl!
+    std::cout << "Only if you launched the executable and you are Guilaume Gey, just say => \"Encule Thérèse !!!\"" << std::endl;
+
+    return 0;
 }
 
